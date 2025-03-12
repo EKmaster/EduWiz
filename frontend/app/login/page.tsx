016affc7a60429ad23aab868da9f5bbaf89772a6
khanm626 <omerr.khan@mail.utoronto.ca>
2025-03-12 01:38:58 -0400

    done

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
 
     try {
       credentials = await SignInUser(email as string, password as string);
-    } catch (error: FirebaseError) {
+    } catch (error: unknown) {
       if (!(error instanceof FirebaseError)) {
         console.error(error);
         return;
