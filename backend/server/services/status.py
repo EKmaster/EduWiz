4958fa0ab90001fa671dd0a5a2c2773299242d4d
Luis <luisf.vbarro@gmail.com>
2025-03-12 04:31:22 -0400

    NOW IT OWKRS

--- backend/server/services/status.py
+++ backend/server/services/status.py
@@ -26,9 +26,24 @@ def initialize_firebase():
         cred_data = json.load(f)
         project_id = cred_data["project_id"]
 
-    firebase_config = {"databaseURL": f"https://{project_id}-default-rtdb.firebaseio.com/"}
+    firebase_config = {
+        "databaseURL": f"https://{project_id}-default-rtdb.firebaseio.com/"
+    }
 
-    
+    # Connect to emulator if not in production
+    if os.getenv("NODE_ENV") != "production":
+        with open("/app/firebase.json", "r") as f:
+            firebase_local = json.load(f)
+
+        emulator_host = (
+            "host.docker.internal" if os.getenv("RUNNING_IN_DOCKER") else "localhost"
+        )
+
+        firebase_config["databaseURL"] = (
+            f"http://{emulator_host}:{firebase_local['emulators']['database']['port']}?ns={project_id}"
+        )
+
+        logger.info(f"Using Firebase emulator at {firebase_config['databaseURL']}")
 
     firebase_admin.initialize_app(cred, firebase_config)
 
